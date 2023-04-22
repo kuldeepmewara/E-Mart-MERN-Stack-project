01b49a3205b7ce2b3fec7cbf62a742b6ed49142a
kuldeep mewara <kuldeepmewara18@gmail.com>
2023-04-22 12:17:16 +0530

    first commit

---
+++ backend/seeder/users.js
@@ -0,0 +1,21 @@
+const bcrypt = require("bcryptjs");
+const ObjectId = require("mongodb").ObjectId;
+
+const users = [
+  {
+    name: "admin",
+    lastName: "admin",
+    email: "admin@admin.com",
+    password: bcrypt.hashSync("admin@admin.com", 10),
+    isAdmin: true,
+  },
+  {
+    _id: ObjectId("625add3d78fb449f9d9fe2ee"),
+    name: "kuldeep",
+    lastName: "mewara",
+    email: "k@m.com",
+    password: bcrypt.hashSync("k@m.com", 10),
+  },
+];
+
+module.exports = users;
